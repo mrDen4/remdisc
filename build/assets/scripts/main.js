97fa7017aeb3ee5717d5de23d6c9ae44956a4b0d
Denis Davydov <den.saint.k@gmail.com>
2018-12-25 14:11:13 +0300

    add slider in works

--- build/assets/scripts/main.js
+++ build/assets/scripts/main.js
@@ -6,4 +6,11 @@ $(document).ready(function () {
     prevArrow: $('.hero__arrow--l'),
     nextArrow: $('.hero__arrow--r')
   });
+
+  $('.works__slider').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    prevArrow: $('.works__arrow--l'),
+    nextArrow: $('.works__arrow--r')
+  });
 });
